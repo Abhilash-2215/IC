9d2a8829bb6f73b3c44819e444eca0204449a7ad
Abhilash-2215 <abhilash81354@gmail.com>
2026-06-12 11:59:00 +0530

    Add files via upload

---
+++ Manipulation.js
@@ -0,0 +1,20 @@
+/*t greet=document.getElementsByClassName("wishes").innerText;
+console.log(greet);*/
+/*t greet=document.getElementById("wish).innerText;
+console.log(greet);*/
+/*t greet=document.getElementsByTagName("h1").innerText;
+console.log(greet);*/
+/*t greet=document.querySelector("#content");
+console.log(greet);*/
+/*t greet=document.querySelectorAll("p").innerText;
+console.log(greet);*/
+let pen =document.getElementById("wish");
+console.log(pen);
+let pencil =document.getElementsByClassName("wishes");
+console.log(pencil);
+let queries=document.querySelectorAll("p");
+console.log(queries);
+let lubber =document.getElementsByTagName("div");
+console.log(lubber);
+let water=document.querySelector("#content");
+console.log(water);
